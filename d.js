8ecba42b8cfdcc9da15f28597823a4933c76dada
Debbie002 <deborahpeter011@gmail.com>
2024-05-24 11:06:54 +0100

    initial commit

---
+++ d.js
@@ -0,0 +1,103 @@
+const form = document.getElementById("form-info")
+
+function getData(){
+    const firstName = document.getElementById("first-name").value
+    const lastName = document.getElementById("last-name").value
+    const otherName = document.getElementById("other-name").value
+    const email = document.getElementById("email").value
+    const phoneNum = document.getElementById("phone-number").value
+    const dropDown = document.getElementById("dropdown").value
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const phonePattern = /^[0-9]{10}$/
+
+    const error = document.querySelector(".error")
+    const error1 = document.querySelector(".error1")
+    const error2 = document.querySelector(".error2")
+    const error3 = document.querySelector(".error3")
+    const error4 = document.querySelector(".error4")
+    const error5 = document.querySelector(".error5")
+
+    const data = {
+        firstName, lastName, otherName, email, phoneNum, dropDown
+    }
+
+
+
+
+    if(firstName === ""){
+        error.textContent = "Enter Firstname"
+    }else if(firstName.length < 1){
+        error.textContent = "Firstname should not be less than 1 character"
+    } else if(/\d/.test(firstName)){
+        error.textContent = "FirstName should not contain numbers"
+    }else{
+        error.textContent = ""
+    }
+    
+
+    if(lastName === ""){
+        error1.textContent = "Enter Lastname"
+    }else if(lastName.length < 1){
+        error1.textContent = "Lastname should not be less than 1 character"
+    } else if(/\d/.test(lastName)){
+        error1.textContent = "LastName should not contain numbers"
+    }else{
+        error1.textContent = ""
+    }
+
+    
+
+    if(email === ""){
+        error3.textContent = "Enter Email"
+    }else if(!emailPattern.test(email)){
+        error3.textContent = "Invalid Email"
+    }else{
+        error3.textContent = ""
+    }
+
+    
+    if(phoneNum === ""){
+        error4.textContent = "Enter Phone Number"
+    }else if(!phonePattern.test(phoneNum)){
+        error4.textContent = "Number Format is 8147510665"
+    }else{
+        error4.textContent = ""
+    }
+
+    if(dropDown === ""){
+        error5.textContent = "Select an Option"
+    }else{
+        error5.textContent = ""
+    }
+
+    try {
+        fetch("http://localhost:3022/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(response =>response.json())
+        .then(data =>{
+            window.location = "database.json"
+            console.log("good", data);
+        })
+        
+    } catch (error) {
+        if(error) throw error;
+    }
+}
+
+
+
+
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    getData()
+   
+
+})
+
+
+
---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require("http")
+const fs = require("fs")
+
+
+const handleRequest = ((req, res)=>{
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(204, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Accept",
+            "Content-Length": 0
+        });
+        res.end();
+        return;
+    }
+
+
+    if(req.method === "POST" && req.url === "/submit"){
+        let body = ""
+        req.on("data", chunk =>{
+            body +=chunk.toString()
+        })
+        req.on("end", ()=>{
+            const data = JSON.parse(body)
+            fs.appendFile("database.json",JSON.stringify(data) + ", \n", (error)=>{
+                if(error){
+                    res.writeHead(500, {"Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                    })
+                    res.end(JSON.stringify({message: "server errror"}))
+                }
+                res.writeHead(200, {"Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+
+                })
+                res.end(JSON.stringify({message: "saved"}))
+
+            })
+
+        })
+    }else{
+        res.writeHead(404, {"Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*"
+
+        })
+        res.end("Not found")
+    }
+
+})
+
+const server = http.createServer(handleRequest)
+
+
+server.listen(3022, ()=>{
+    console.log("server running");
+})
